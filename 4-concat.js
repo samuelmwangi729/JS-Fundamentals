4cd3451c700a0c48c9332a055ab6dc1552ad6b1e
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2025-07-13 12:02:21 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,1 @@
-#!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-    console.log('No argument');
-} else {
-    console.log(args[0] + " is " + args[1])
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
