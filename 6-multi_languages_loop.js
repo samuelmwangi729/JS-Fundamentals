959c668460e689899bf5c07ddbe755e7b85925f5
Samuel Mwangi <42680795+samuelmwangi729@users.noreply.github.com>
2025-07-13 12:00:36 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,8 +1,5 @@
-#!/usr/bin/node
-
-const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
 for (let i = 0; i < lines.length; i++) {
-    console.log(i)
+  console.log(lines[i]);
 }
